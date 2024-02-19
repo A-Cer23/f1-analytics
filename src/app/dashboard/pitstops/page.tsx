44b236ad15ed94115fa1fbb5682734810681bc27
A-Cer23 <85203799+A-Cer23@users.noreply.github.com>
2024-02-19 15:23:52 -0500

    Create seperation of dashboard page and pitstop page

--- src/app/dashboard/pitstops/page.tsx
+++ src/app/dashboard/pitstops/page.tsx
@@ -1,11 +1,15 @@
 import {fetchPitStops} from "@/app/services/pitstops/service";
 
+
+
+
 export default async function Page() {
 
+  const results = await fetchPitStops();
 
   return (
     <div>
-      <h1>/dashboard/page</h1>
+      {JSON.stringify(results)}
     </div>
   );
-}
+};
