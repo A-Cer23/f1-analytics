fe4067d6e23aca68fd0e9a20b31269588f7196c5
A-Cer23 <85203799+A-Cer23@users.noreply.github.com>
2024-02-19 15:24:41 -0500

    Fix next config for local development

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'development') {
     bindings: {
       D1DB: {
         type: 'd1',
-        databaseId: '27bef208-ee78-46dd-993d-d74802cea736'
+        databaseId: 'D1DB'
       },
     },
   });
