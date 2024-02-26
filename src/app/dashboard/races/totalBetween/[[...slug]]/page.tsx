093f504e6ba4aefe433c5158a17c9a8339d4e3c5
A-Cer23 <85203799+A-Cer23@users.noreply.github.com>
2024-02-26 16:20:38 -0500

    Configure for edge runtime

--- src/app/dashboard/races/totalBetween/[[...slug]]/page.tsx
+++ src/app/dashboard/races/totalBetween/[[...slug]]/page.tsx
@@ -2,6 +2,8 @@ import ChartTotalRacesBetween from "@/app/ui/charts/races/chartTotalRacesBetween
 import {redirect} from "next/navigation";
 
 
+export const runtime = 'edge';
+
 export default function Page({params}: {params: {slug: string[]}}) {
 
   if (Object.keys(params).length === 0 || params.slug.length < 2) {
